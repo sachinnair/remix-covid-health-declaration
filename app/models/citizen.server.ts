0129205664b09f7356501df1a27f58b2a59c3fd9
Sachin <sachinkn6@gmail.com>
2022-09-21 12:24:24 +0800

    Model Citizen revised

--- app/models/citizen.server.ts
+++ app/models/citizen.server.ts
@@ -1,5 +1,8 @@
 import { prisma } from "~/db.server";
+import type { Citizen } from '@prisma/client';
 
-export const createCitizen = async ({ data }) => {
-  return await prisma.citizen.create({ data });
+export type TCitizen = Required<Pick<Citizen, "name"|"temperature">> & Partial<Pick<Citizen, "isDegreeCelsius">>
+
+export const createCitizen = async ({ data }: { data: TCitizen }) => {
+  return await prisma.citizen.create({ data: (data as Citizen) });
 }
