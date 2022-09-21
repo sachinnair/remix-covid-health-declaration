902e6e1ead9dece0442976d86c426cfe7adae014
Sachin <sachinkn6@gmail.com>
2022-09-21 12:43:25 +0800

    [IMP] Test setup issue - check latest to remove patch

--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -2,3 +2,7 @@ import { installGlobals } from "@remix-run/node";
 import "@testing-library/jest-dom/extend-expect";
 
 installGlobals();
+
+global.HTMLElement.prototype.detachEvent = function(type, listener) {
+    this.removeEventListener(type.replace('on', ''), listener)
+  }
