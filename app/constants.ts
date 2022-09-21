ca2aa12e086d3e125583ffa9eed5378bcfc6e50f
Sachin <sachinkn6@gmail.com>
2022-09-21 12:35:06 +0800

    Symptoms section

---
+++ app/constants.ts
@@ -0,0 +1,12 @@
+export const SYMPTOMS: Record<string, string> = {
+    Cough: 'Cough', 
+    Sti: 'Smell / Taste Impairment', 
+    Fever: 'Fever',
+    BreathDiffi: 'Breathing Difficulties', 
+    BodyAches: 'Body Aches', 
+    Headaches: 'Headaches', 
+    Fatigue: 'Fatigue', 
+    SoreThroat: 'Sore Throat', 
+    Diarrhoea: 'Diarrhoea', 
+    RunnyNose: 'Runny Nose',
+}
