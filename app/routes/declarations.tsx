9484f608da749830e65b57a3c4cadc998fd339c7
Sachin <sachinkn6@gmail.com>
2022-09-21 12:36:41 +0800

    App Header

--- app/routes/declarations.tsx
+++ app/routes/declarations.tsx
@@ -1,7 +1,19 @@
-import { Outlet } from '@remix-run/react'
+import { Outlet } from "@remix-run/react";
+import { Text } from "@chakra-ui/react";
+
 export default function Declarations() {
-    return (<main>
-        <h1>Covid-19 Declaration Form</h1>
-        <Outlet />
-    </main>)
+  return (
+    <main>
+      <div className="m-5 sm:m-20 sm:mb-10">
+        <Text as="b" fontSize="4xl">
+          COVID-19
+        </Text>
+        <br />
+        <Text as="b" fontSize="4xl">
+          HEALTH DECLARATION
+        </Text>
+      </div>
+      <Outlet />
+    </main>
+  );
 }
